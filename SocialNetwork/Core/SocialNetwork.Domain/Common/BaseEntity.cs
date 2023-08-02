236da7c67575dfa10f6b9b6181d84b97c462f056
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-08-02 12:23:30 +0300

    domain created. The entities and dtos have been created.

---
+++ SocialNetwork/Core/SocialNetwork.Domain/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialNetwork.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string CreatedByName { get; set; }
+    public DateTime LastModifiedDate { get; set; }
+    public string LastModifiedBy { get; set; }
+}
---
+++ SocialNetwork/Core/SocialNetwork.Domain/Dtos/User/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialNetwork.Domain.Dtos.User;
+
+public class UserDto : BaseDto
+{
+    public string UserName { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+}
